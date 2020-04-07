b3d349df3f31f0c1b975cfaca6ef6e931681efa1
rohan <rshanker779@gmail.com>
2020-04-07 23:48:33 +0100

    Removing dodgy code

--- docsrc/source/modules.rst
+++
@@ -1,7 +0,0 @@
-rshanker779_common
-==================
-
-.. toctree::
-   :maxdepth: 4
-
-   rshanker779_common
--- rshanker779_common/system.py
+++
@@ -1,9 +0,0 @@
-import getpass
-import socket
-import platform
-
-
-class System:
-    HOSTNAME = socket.gethostname()
-    USERNAME = getpass.getuser()
-    PLATFORM = platform.system()
